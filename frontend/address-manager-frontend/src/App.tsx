b90caa66ff2166f64f583eefcffb06fe761dee7f
vagnersantosdasilva <vagnersantosdasilva@gmail.com>
2026-03-25 09:49:26 -0300

    feat:initial commit frontend

---
+++ frontend/address-manager-frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+  
+    </>
+  )
+}
+
+export default App
